2623628d7dc31024e1aa031d67c7d049b381b667
Younes <younesetud@gmail.com>
2023-10-14 14:51:33 +0100

    Added .env file

--- image_processing.py
+++ image_processing.py
@@ -1,57 +1,68 @@
+import os
+
 import cv2 as cv
 import pytesseract
+from dotenv import load_dotenv
+
+load_dotenv()
+teseract_path = os.getenv("TESSERACTS_PATH")
+upload_path = os.path.join('static', 'images')
 
 
-teseract_path = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
-
-# Read Image 
+# Read Image
 def read_Img(path):
-  image_original = cv.imread(path)
-  return image_original
+    image_original = cv.imread(path)
+    return image_original
+
 
 # Convert image to Gray
 def to_gray_Img(img):
-    image_gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+    image_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     return image_gray
+
 
 # Gaussian filtering
 def gaussian_Filt(image):
-  image_gauss = cv.GaussianBlur(image,(5,5),0)
-  return image_gauss
+    image_gauss = cv.GaussianBlur(image, (5, 5), 0)
+    return image_gauss
+
 
 # Otsu's thresholding
 def otsu_Thresh(image):
-  otsu_threshold, image_result = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
-  return image_result
+    otsu_threshold, image_result = cv.threshold(image, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+    return image_result
 
-# Get Image dimensions 
+
+# Get Image dimensions
 def get_Img_Dim(image):
-  dimensions = image.shape
-  height = image.shape[0]
-  width = image.shape[1]
-  print('Image Dimension    : ',dimensions)
-  print('Image Height       : ',height)
-  print('Image Width        : ',width)
+    dimensions = image.shape
+    height = image.shape[0]
+    width = image.shape[1]
+    print('Image Dimension    : ', dimensions)
+    print('Image Height       : ', height)
+    print('Image Width        : ', width)
 
-# Save image 
-def save_Img(image,name,type):
-  path = 'D:\\proje\\OCR\\images\\'+name+'.'+type
-  isWritten = cv.imwrite(path, image)
-  if isWritten:{
-	  print('Image is successfully saved.\n')}
-  return path 
+
+# Save image
+def save_Img(image, name, type):
+    path = upload_path + name + '.' + type
+    isWritten = cv.imwrite(path, image)
+    if isWritten: {
+        print('Image is successfully saved.\n')}
+    return path
 
 
 # Resize Image 
-def resize_Img(img,scale_percent):
-  width = int(img.shape[1] * scale_percent / 100)
-  height = int(img.shape[0] * scale_percent / 100)
-  dim = (width, height)
-  resized_img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
-  return resized_img
+def resize_Img(img, scale_percent):
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    resized_img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
+    return resized_img
+
 
 # Extract text from the image  ( ara , eng )
-def extract_Text(image,language):
-  pytesseract.pytesseract.tesseract_cmd = teseract_path
-  text = pytesseract.image_to_string(image, lang=language)
-  return text
+def extract_Text(image, language):
+    pytesseract.pytesseract.tesseract_cmd = teseract_path
+    text = pytesseract.image_to_string(image, lang=language)
+    return text
--- myapp.py
+++ myapp.py
@@ -1,66 +1,73 @@
 import os
-from flask import Flask , render_template ,request 
+from flask import Flask, render_template, request
 # from werkzeug.utils import secure_filename
 import backend_functions as bf
+from dotenv import load_dotenv
 
 UPLOAD_FOLDER = os.path.join('static', 'images')
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'jpe' , 'webp'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'jpe', 'webp'}
 
 myapp = Flask(__name__)
 myapp.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-myapp.secret_key = "12345abcde"
+myapp.secret_key = os.environ.get("SECRET_KEY")
+
 
 @myapp.route("/")
 @myapp.route("/home")
 @myapp.route("/index")
 def index():
-    return render_template("index.html",pagetitle="Main page")
+    return render_template("index.html", pagetitle="Main page")
+
 
 @myapp.route("/contact")
 def contact():
-    return render_template("contact.html",pagetitle="Contact page")
+    return render_template("contact.html", pagetitle="Contact page")
+
 
 @myapp.route("/about")
 def about():
-    return render_template("about.html",pagetitle="About page")
+    return render_template("about.html", pagetitle="About page")
 
-@myapp.route("/to_Black_White",methods=['GET', 'POST'])
+
+@myapp.route("/to_Black_White", methods=['GET', 'POST'])
 def to_black_white():
     if request.method == 'POST':
-        _ ,img_path = bf.upload_file(request,ALLOWED_EXTENSIONS,myapp.config['UPLOAD_FOLDER'])
+        _, img_path = bf.upload_file(request, ALLOWED_EXTENSIONS, myapp.config['UPLOAD_FOLDER'])
         if _:
             img_path2 = bf.img_2_bw(img_path)
-            return render_template("bnw_page.html",pagetitle="B&W Image page",
-                                        orgnl_image_path=img_path,
-                                        bnw_image_path=img_path2)
+            return render_template("bnw_page.html", pagetitle="B&W Image page",
+                                   orgnl_image_path=img_path,
+                                   bnw_image_path=img_path2)
         else:
-            return render_template("bnw_page.html",pagetitle="B&W Image page")
+            return render_template("bnw_page.html", pagetitle="B&W Image page")
     else:
-        return render_template("bnw_page.html",pagetitle="B&W Image page")
+        return render_template("bnw_page.html", pagetitle="B&W Image page")
 
-@myapp.route("/extract_text",methods=['GET', 'POST'])#############################rani hna 
+
+@myapp.route("/extract_text", methods=['GET', 'POST'])
 def extract_text():
     if request.method == 'POST':
-        _ ,img_path = bf.upload_file(request,ALLOWED_EXTENSIONS,myapp.config['UPLOAD_FOLDER'])
+        _, img_path = bf.upload_file(request, ALLOWED_EXTENSIONS, myapp.config['UPLOAD_FOLDER'])
         chosen_language = request.form.get("lang")
-        if _ :
-            extrcted_text = bf.img_2_text(img_path,chosen_language)
-            return render_template("extracttext_page.html",pagetitle="Text page",
-                                        orgnl_image_path=img_path,
-                                        extrt_txt = extrcted_text) 
+        if _:
+            extrcted_text = bf.img_2_text(img_path, chosen_language)
+            return render_template("extracttext_page.html", pagetitle="Text page",
+                                   orgnl_image_path=img_path,
+                                   extrt_txt=extrcted_text)
         else:
-            return render_template("extracttext_page.html",pagetitle="Text page")
+            return render_template("extracttext_page.html", pagetitle="Text page")
     else:
-        return render_template("extracttext_page.html",pagetitle="Text page")
+        return render_template("extracttext_page.html", pagetitle="Text page")
 
 
-@myapp.route("/download_image",methods=['GET','POST'])
+@myapp.route("/download_image", methods=['GET', 'POST'])
 def download_image():
     if request.method == 'POST':
-        name = request.form.get("bnwtest","(not working)")
-        img_path = os.path.join(myapp.config['UPLOAD_FOLDER'], name)
-        return bf.dowload_file(img_path)
+        name = request.form.get("bnwtest", "(not working)")
+        # img_path = os.path.join(myapp.config['UPLOAD_FOLDER'], name)
+        return bf.dowload_file(myapp.config['UPLOAD_FOLDER'], name)
 
 
 if __name__ == "__main__":
+    load_dotenv()
     myapp.run(debug=True)
